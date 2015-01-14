ef68212bce41b16f9348ab141ea4119b33b6cfde
Jef Mallal <jef.mallal@hp.com>
2015-01-14 13:59:13 -0700

    Game of life.

---
+++ tdd/gameoflife/gameoflife.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+from collections import defaultdict
+import time
+
+
+class Board:
+    def __init__(self):
+        self.board = defaultdict(lambda: 0)
+
+    def addCells(self, cells):
+        # TODO check not override another cell in location
+        # and doesn't go out of bounds
+        # or find another way to initialize board
+        for cell in cells:
+            self.board[cell] = 1
+            self.getNeighborCount(cell) # realize neighbor cells
+
+    def getState(self, cell):
+        return self.board[cell]
+
+    def getNeighborCount(self, cell):
+        a, b = cell
+        neighbors = [self.board[(x, y)] for x in range(a-1, a+2)
+                                        for y in range(b-1, b+2)
+                                        if (x, y) != (a, b)]
+        return sum(neighbors)
+
+    def lifeMechanic(self, neighborCount, cellState):
+        if neighborCount == 2 and cellState == 1:
+            return 1
+        if neighborCount == 3:
+            return 1
+        return 0
+
+    def advancegame(self):
+        nextBoard = Board()
+        for cell in self.board.keys():
+            if self.lifeMechanic(self.getNeighborCount(cell), self.board[cell]) == 1:
+             nextBoard.addCells([cell])
+        self.board = nextBoard.board
+
+    def printBoard(self, x, y):
+        while True:
+            printable = ""
+            for a in range(-x, x):
+                for b in range(-y, y):
+                    if self.board[(a, b)] == 1:
+                        printable += "#"
+                    else:
+                        printable += " "
+                    printable += " "
+                printable += "\n"
+            print printable
+            self.advancegame()
+            time.sleep(1)
+            print
+
+def main():
+    board = Board()
+    board.addCells([(1, 1), (1, 2), (1, 3),
+                    (2, 1), (2, 2), (2, 3),
+                    (3, 1), (3, 2), (3, 3)])
+    # Ocillator
+    #board.addCells([(1, 1), (1, 2), (1, 3)])
+    # Glider
+    #board.addCells([(0, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
+    #board.addCells([(4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8)])
+    board.printBoard(9, 9)
+
+if __name__ == "__main__":
+    main()
--- tdd/gameoflife/testgame.py
+++ tdd/gameoflife/testgame.py
@@ -13,69 +13,108 @@
 #        test-friendly way, and what "value" cells outside the board has.
 #        Or maybe the board does not have borders?
 import unittest2
-from negativizer import Negativizer
-
-
-class Board:
-    def __init__(self):
-        self.cells = {}
-
-    def getKey(self, cell):
-        return "%s %s" % cell
-
-    def getCellAt(self, x, y):
-        return self.cells[self.getKey((x, y))]
-
-    def addCells(self, cells):
-        # TODO check not override another cell in location
-        for cell in cells:
-            self.cells[self.getKey(cell)] = cell
-
-    def animated(self, cell):
-        if self.getKey(cell) in self.cells:
-            return True
-
-    def advancegame(self, cell):
-        self.cells = {}
+from gameoflife import Board
 
 
 class TestGameOfLife(unittest2.TestCase):
     def setUp(self):
         self.board = Board()
-        self.Not = Negativizer(self)
 
     def tearDown(self):
         self.board = None
 
-    def assertAlive(self, cell):
-        if not self.board.animated(cell):
-            self.fail("Expected life")
+    def assertState(self, state, cell):
+        self.assertEquals(self.board.getState(cell), state)
 
-    def assertDies(self, cell):
-        self.assertAlive(cell)
-        self.board.advancegame(cell)
-        self.Not.assertAlive(cell)
+    def assertStateChange(self, startState, endState, cell):
+        self.assertState(startState, cell)
+        self.board.advancegame()
+        self.assertState(endState, cell)
 
-    def testAlivebyDefault(self):
+    def assertNeighbors(self, cell, expectedNeighborCount):
+        neighborCount = self.board.getNeighborCount(cell)
+        if neighborCount != expectedNeighborCount:
+            self.fail("Expected %s neighbors, got %s"
+                      %(expectedNeighborCount, neighborCount))
+
+    def testAliveifAdded(self):
         cell = (0, 0)
         self.board.addCells([cell])
-        self.assertAlive(cell)
+        self.assertState(1, cell)
+
+    def testDeadCellisDead(self):
+        self.assertState(0, (-1,-1))
 
     def test2Cells(self):
         cell1 = (0, 0)
         cell2 = (100000, 1)
-        self.Not.assertAlive(cell1)
-        self.Not.assertAlive(cell2)
+        self.assertState(0, cell1)
+        self.assertState(0, cell2)
         self.board.addCells([cell1, cell2])
-        self.assertAlive(cell1)
-        self.assertAlive(cell2)
+        self.assertState(1, cell1)
+        self.assertState(1, cell2)
 
     def testLifeExpiresSingleCell(self):
-        #Any live cell with fewer than two live neighbours dies,
+        # Any live cell with fewer than two live neighbours dies,
         #    as if caused by underpopulation.
         cell = (0, 0)
         self.board.addCells([cell])
-        self.assertDies(cell)
+        self.assertStateChange(1, 0, cell)
 
-    def testNeighborCount(self):
-        self.fail()
+    def testCellWithMoreThanThreeNeighborsDies(self):
+        # Any live cell with more than three live neighbours dies,
+        #     as if by overcrowding.
+        self.board.addCells([(0, 0), (0, 1), (0, 2),
+                             (1, 0), (1, 1)])
+        self.assertStateChange(1, 0, (1, 1))
+
+    def testCellTwoNeighborsLives(self):
+        # Any live cell with two or three live neighbours lives
+        #     on to the next generation.
+        self.board.addCells([(0, 0), (0, 1), (0, 2)])
+        self.assertStateChange(1, 1, (0, 1))
+
+    def testDead2NeighborsStaysDead(self):
+        self.board.addCells([(0, 0), (0, 1)])
+        self.assertStateChange(0, 0, (1, 0))
+
+    def testDeadExactly3NeighborsAnimates(self):
+        # Any dead cell with exactly three live neighbours becomes a live cell.
+        self.board.addCells([(0, 0), (0, 1), (0, 2)])
+        self.assertStateChange(0, 1, (1, 1))
+
+    def testCellThreeNeighborsLives(self):
+        self.board.addCells([(0, 0), (0, 1), (0, 2),
+                             (1, 0), (1, 1)])
+        self.assertStateChange(1, 1, (1, 0))
+
+    def testNeighborCountOne(self):
+        cell1 = (0, 0)
+        cell2 = (0, 1)
+        self.board.addCells([cell1, cell2])
+        self.assertNeighbors(cell1, 1)
+
+    def testNeighborCountTwo(self):
+        cell1 = (0, 0)
+        cell2 = (0, 1)
+        cell3 = (1, 0)
+        self.board.addCells([cell1, cell2, cell3])
+        self.assertNeighbors(cell1, 2)
+
+    def testNeighborCountAll(self):
+        self.board.addCells([(1, 1), (1, 2), (1, 3),
+                             (2, 1), (2, 2), (2, 3),
+                             (3, 1), (3, 2), (3, 3)])
+        self.assertNeighbors((2, 2), 8)
+
+    def testAFewStates(self):
+        self.board.addCells([(0, 0), (0, 1), (0, 2)])
+        self.board.advancegame()
+        self.assertState( 1, (-1, 1))
+        self.board.advancegame()
+        self.assertState(1, (0, 0))
+
+    def testAfewMore(self):
+        self.board.addCells([(0, 0), (0, 1), (0, 2)])
+        self.assertStateChange(0, 1, (-1, 1))
+        self.assertStateChange(0, 1, (0, 0))
