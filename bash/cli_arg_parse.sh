c64d64c6be0d2a4592298e905265d8b7dd44af90
Jef Mallal <jef.mallal@hp.com>
2015-03-19 10:57:48 -0600

    Added repeatable command to cli_arg_parse.sh

--- bash/cli_arg_parse.sh
+++ bash/cli_arg_parse.sh
@@ -13,6 +13,7 @@ usage() {
     OPTIONS:
         -a	Option a
         -b	Option b
+        -c	Option c
         -h	show this help
 
     EXAMPLE:
@@ -20,13 +21,14 @@ usage() {
         Output:
             OPTION_A: something
             OPTION_B: other
+            OPTION_C: repeatable args
             Extra Arguments: more command line args
 EOF
 }
 
 cmdline() {
     local OPTIND=1
-    while getopts "ha:b:" OPTION
+    while getopts "ha:b:c:" OPTION
     do
         case $OPTION in
             h)
@@ -39,6 +41,9 @@ cmdline() {
             b)
                 OPTION_B=${OPTARG}
                 ;;
+            c)
+                OPTION_C+=("$OPTARG")
+                ;;
         esac
     done
     shift $((OPTIND-1))
@@ -46,7 +51,7 @@ cmdline() {
 }
 
 main() {
-    local OPTION_A OPTION_B extras
+    local OPTION_A OPTION_B OPTION_C extras
     cmdline $ARGS
     if [[ -n $OPTION_A ]]; then
         echo 'OPTION_A: '$OPTION_A
@@ -54,7 +59,15 @@ main() {
     if [[ -n $OPTION_B ]]; then
         echo 'OPTION_B: '$OPTION_B
     fi
-    echo 'Extra Arguments: '$extras
+    local OPTION_C_apend
+    for val in "${OPTION_C[@]}"; do
+        OPTION_C_append+=("-c $val")
+    done
+    if [[ -n $OPTION_C ]]; then
+        echo 'OPTION_C: '${OPTION_C[@]}
+        echo 'OPTION_C_append: '${OPTION_C_append[@]}
+    fi
+    echo 'Extra Arguments: '${extras[@]}
 }
 
 main
