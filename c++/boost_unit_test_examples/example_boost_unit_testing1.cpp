e2f7c26de6b8723e446744e4a8363edc766335e8
mallal <jef.mallal@hp.com>
2016-03-28 14:22:28 -0600

    Add C++ boost unit test examples

---
+++ c++/boost_unit_test_examples/example_boost_unit_testing1.cpp
@@ -0,0 +1,47 @@
+// Example compile $<compiler> <thisFileName> -lboost_unit_test_framework
+
+// Needed for dynamic linking
+#define BOOST_TEST_DYN_LINK
+// This could be used to define this as the main test module instead of a named module
+//#define BOOST_TEST_MAIN
+// Define test suite module name as "test something"
+#define BOOST_TEST_MODULE test something example 1
+#include <boost/test/unit_test.hpp>
+// Use #include <boost/test/included/unit_test.hpp> for static linking
+
+// Setup and teardown fictures
+#include <iostream>
+struct F
+{
+    F() { std::cout << "setup 1" << std::endl; }
+    ~F() { std::cout << "teardown 1" << std::endl; }
+};
+// Define F as global fixture, going to leave commented out for less output for now
+//BOOST_GLOBAL_FIXTURE( F );
+
+// Example successful unit tests
+// automatic test case registration
+BOOST_AUTO_TEST_CASE( test1 )
+{
+    BOOST_CHECK( 1 == 1 );
+    BOOST_CHECK( 1 == 2 );
+    BOOST_CHECK( 1 == 3 );
+}
+
+struct F2
+{
+    F2() { std::cout << "setup 2" << std::endl; }
+    ~F2() { std::cout << "teardown 2" << std::endl; }
+};
+// Example failure unit test
+// each test file may contain any number of test cases; each test case has to have unique name
+// Define F2 as local test fixture
+BOOST_FIXTURE_TEST_CASE( test2, F2 )
+{
+    int i = 1;
+    BOOST_CHECK_EQUAL( i, 1 );
+    // Failure here means entire "test2" is a failure
+    // Each BOOST_AUTO_TEST_CASE is considered an individual test
+    //  each check within the test is not part of the overall test
+    BOOST_CHECK_EQUAL( i, 2 );
+}
---
+++ c++/boost_unit_test_examples/example_boost_unit_testing2.cpp
@@ -0,0 +1,27 @@
+// Takes a long time to compile with the included lib
+#include <boost/test/included/unit_test.hpp>
+using namespace boost::unit_test;
+
+int return_one()
+{
+    return 1;
+}
+
+// Need to register unit test function
+void test_something()
+{
+    BOOST_CHECK_EQUAL(return_one(), 2);
+    BOOST_REQUIRE_EQUAL(return_one(), 2);
+}
+
+test_suite* init_unit_test_suite( int, char* [] )
+{
+    // Test suite name
+    framework::master_test_suite().p_name.value = "Test something example 2";
+
+    // Register test_something
+    // optional: # of expected failing tests
+    framework::master_test_suite().add( BOOST_TEST_CASE( &test_something ), 0 );
+
+    return 0;
+}
