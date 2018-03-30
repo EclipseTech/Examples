c6b065ac243721444fd72e8ef61450cd0d261518
Jef Mallal <jef.mallal@hpe.com>
2018-03-30 16:17:50 -0600

    Fix spacing issue in click basic

--- python/cli/clickBasic.py
+++ python/cli/clickBasic.py
@@ -6,13 +6,13 @@ import click
 def greet():
     pass
 
-    @greet.command()
-    def hello(**kwargs):
-        pass
+@greet.command()
+def hello(**kwargs):
+    pass
 
-    @greet.command()
-    def goodbye(**kwargs):
-        pass
+@greet.command()
+def goodbye(**kwargs):
+    pass
 
 if __name__ == '__main__':
     greet()
