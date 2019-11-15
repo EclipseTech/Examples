75ca4221bac85457f4559dcdd7d93bb252e7eef8
Jef Mallal <jef.mallal@hpe.com>
2019-11-14 18:09:19 -0700

    Add advanced log handling

--- python/logging/7_advanced_log_handling.py
+++ python/logging/7_advanced_log_handling.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     # Setup logging
     log = logging.getLogger(__name__)
     stream_handler = logging.StreamHandler()
-    stream_formatter = logging.Formatter('%(levelname)-8s:{} %(message)s'.format(__name__))
+    stream_formatter = logging.Formatter('%(levelname)-8s{}: %(message)s'.format(__name__))
     stream_handler.setFormatter(stream_formatter)
     log.addHandler(stream_handler)
     log.setLevel(logging.INFO)
@@ -14,3 +14,11 @@ if __name__ == '__main__':
     log.info('I setup my log nicely')
     hog = log_hog.i_am_a_log_hog()
     hog.do_something()
+
+    log.error('Shut up log hog! You should not print anything to my console!')
+
+    log_hog_logger = logging.getLogger(log_hog.__name__)
+    log_hog_logger.disabled = True
+    hog.do_something()
+
+    log.info('Good log hog is no longer a log hog')
--- python/logging/log_hog.py
+++ python/logging/log_hog.py
@@ -3,13 +3,13 @@ import logging
 
 class i_am_a_log_hog():
     def __init__(self):
-        log = logging.getLogger(__name__)
+        self.log = logging.getLogger(__name__)
         stream_handler = logging.StreamHandler()
-        stream_formatter = logging.Formatter('%(levelname)-8s:{} %(message)s'.format(__name__))
+        stream_formatter = logging.Formatter('%(levelname)-8s{}: %(message)s'.format(__name__))
         stream_handler.setFormatter(stream_formatter)
-        log.addHandler(stream_handler)
-        log.setLevel(logging.DEBUG)
-        log.debug('I set up my log nicely')
+        self.log.addHandler(stream_handler)
+        self.log.setLevel(logging.DEBUG)
+        self.log.debug('I set up my log nicely')
 
     def do_something(self):
-        log.debug('I am doing something and logging to everyones console')
+        self.log.debug('I am doing something and logging to everyones console')
